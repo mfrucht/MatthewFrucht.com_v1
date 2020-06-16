1e6644a4a429eb6ce0559aa734b9240e6762d835
Matthew Scott Frucht <msf239@cornell.edu>
2020-06-16 14:41:30 -0400

    updated resume, begun adding books page

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,8 +5,9 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-plugin-offline-app-shell-js": hot(preferDefault(require("/Users/matthewfrucht/Desktop/Personal_Website/node_modules/gatsby-plugin-offline/app-shell.js"))),
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/matthewfrucht/Desktop/Personal_Website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/matthewfrucht/Desktop/Personal_Website/src/pages/404.js"))),
+  "component---src-pages-books-js": hot(preferDefault(require("/Users/matthewfrucht/Desktop/Personal_Website/src/pages/books.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/matthewfrucht/Desktop/Personal_Website/src/pages/index.js")))
 }
 
---
+++ src/pages/books.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const BooksPage = () => (
+  <Layout>
+    <SEO title="Books" />
+  </Layout>
+)
+
+export default BooksPage
