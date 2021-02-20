83b3a94129dd332f2b85094de9e6a2c85f311463
Matthew Frucht <msf325@gmail.com>
2021-02-20 13:10:01 -0500

    Updated reading page

--- src/pages/books.js
+++ src/pages/books.js
@@ -40,22 +40,45 @@ class BooksPage extends React.Component {
     <Layout>
       <SEO title="Books" />
       <Link style={{marginLeft:0}} to="/" >Back to Home</Link>
-      <p>
-      <br/>
-        This is a little spreadsheet of the books that I've read since starting this
-        page in Spring 2020.
+      <p className="booksP">
+        Hi there! I started this page in Spring 2020 to log my reading (and 
+        to learn how to load data from a Google Sheets spreadsheet). It was a fun
+        little experiment, but I'm going to stop updating this page. I'm doing this
+        for the same reason that I'm going to stop using Goodreads; I don't 
+        particularly like publishing the books that I'm reading, as it creates this
+        sort of "reading for an audience" mindset that I don't like. Every time
+        I'd pick up a title, I would subconsciously thing <i>what are people going to think of
+        me reading this book?</i> 
+
         <br/>
-        I also read <i>The New York Times</i> every day and 
-        <i>The New Yorker</i> every week.
         <br/>
-        As if my to-read list isn't long enough already, I'll take book recommendations
-        sent to <a href="mailto:msf239@cornell.edu">msf239@cornell.edu</a>!
-      </p>
+        
+        To me, that's a terrible mindset to have, as reading
+        is something I do solely for my own pleasure. I also found that publishing
+        my reading list dissuaded me from abandoning books; I didn't want my reading
+        list to show "not completed" or to have to remove books from the list.
+        But I see nothing wrong with abandoning books! books are not contracts, and I 
+        shouldn't have to waste my time finishing a book I dislike simply to check
+        it off as completed. Perhaps that's not the most virtuous view of, but as
+        I said, I read only for my own enjoyment.
+        <br/>
+        <br/>
+        I'm still leaving this page up. So here are the books that I either read
+        or started reading from April 2020 through January 2021:
+        <br/>
+        <br/>
+        </p>
+        
      {loading ? (
         <p>Loading ...</p>
       ) : (
         <Table data={bookData}></Table>
       )}
+
+      <p>
+        I also read <i>The New York Times</i> most days and 
+        <i>The New Yorker</i> every week.
+      </p>
       </Layout>
 
     ) 
